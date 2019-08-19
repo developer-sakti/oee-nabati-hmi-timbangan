a626ef55d0794e8b7e72d9bd6a7ab96e0e9142da
Mojila <me.mojila@gmail.com>
2019-08-19 13:39:45 +0700

    add API_URL ENV

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     API_MAIN: axios.create({
-        baseURL: 'http://ec2-13-250-42-181.ap-southeast-1.compute.amazonaws.com:8081/api/v1/',
+        baseURL: process.env.API_URL,
         timeout: 30000
     })
 }
